00d330c80026e89dfcd64b816301a5c938de7f5c
Adrian Farnell <adrianjfarnell@gmail.com>
2022-05-31 14:02:36 +0100

    code doesn't need the filter!!

--- thing.ps1
+++ thing.ps1
@@ -9,18 +9,32 @@ $myJSONresult = invoke-webrequest https://www.gov.uk/bank-holidays.json
 #write-output $myJSONresult.content
 
 $myInvokeJSONreqResult = invoke-restmethod https://www.gov.uk/bank-holidays.json
+#write-output $myInvokeJSONreqResult.scotland.events
 
-$i = 0
+$counter = 0
 
 foreach ($event in $myInvokeJSONreqResult.scotland.events)
 {
-
+    #write-output "$counter : Suzanne is Great"
     #$event.date is a string so we need to convert it to a string to do a proper comparison
-    $myDate = [datetime]::ParseExact($event.date, 'yyyy-MM-dd', $null)
-    if ($myDate -gt [datetime]::ParseExact('2022-01-01', 'yyyy-MM-dd', $null))
+    
+    $myDataDate = $event.date
+    
+    if ($myDataDate -gt '2022-01-01')
     {
-        write-output "$i :  $($event.date)"
-        $i = $i + 1
+        write-output "$counter : $myDataDate"
     }
+
+    
+    
+    #$myDate = [datetime]::ParseExact($event.date, 'yyyy-MM-dd', $null)
+
+    #if ($myDate -gt [datetime]::ParseExact('2022-01-01', 'yyyy-MM-dd', $null))
+    #{
+        #write-output "$i :  $($event.date)"
+       # $i = $i + 1
+    #}
+    
+    $counter = $counter + 1
 }
 
